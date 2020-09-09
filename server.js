ecfa9d14e187d7fa22bdff94917246a9af067443
Vasileios Drosatos <b.drosat@hotmail.com>
2020-09-09 23:29:38 +0300

    Redirect http to https

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
+app.use(function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
